d785b18eee2b05901209ccaac419fb7a4b04fb24
Cesar Santos <C3S4R780@gmail.com>
2023-08-23 18:31:50 -0300

    [Multi] Changes and additions to the post page - [Add] Button to go back one page - [Add] Sticky sidebar with link sharing options - [Change] Moved post title and excerpt to be on top of the thumbnail

--- frontend/src/components/pages/Post.jsx
+++ frontend/src/components/pages/Post.jsx
@@ -3,6 +3,8 @@ import { Link, useParams } from 'react-router-dom'
 import { ReactMarkdown } from 'react-markdown/lib/react-markdown'
 import style from './Post.module.css'
 
+import ShareSidebar from '../filter/ShareSidebar'
+
 import { FaArrowLeft } from 'react-icons/fa6'
 
 function Post() {
@@ -31,39 +33,44 @@ function Post() {
     "/no-image.png"
 
   return (
-    <div className={style.post}>
-      <div className={style.post_back} onClick={() => window.history.back()}>
-        <FaArrowLeft />
-        <span>Voltar</span>
-      </div>
-      <div className={style.post_title}>
-        <h1>{post.title}</h1>
-      </div>
-      {post.excerpt && (
-        <div className={style.post_excerpt}>
-          <q>{post.excerpt}</q>
+    <div className={style.post_container}>
+      <div className={style.post}>
+        <div className={style.post_back} onClick={() => window.history.back()}>
+          <FaArrowLeft />
+          <span>Voltar</span>
         </div>
-      )}
-      <div className={style.post_thumbnail}>
-        <img src={thumbnail} alt={post.thumbnail?.data?.attributes.alternativeText} />
-      </div>
-      <div className={style.post_info}>
-        <div className={style.post_platforms}>
-          <h3>Plataformas:</h3>
-          {post.platforms && post.platforms?.data.map(platform => (
-            <Link key={`platform-${platform.id}`} to={`/platform/${platform.attributes.name.toLowerCase()}`}>
-              {platform.attributes.name}
-            </Link>
-          ))}
+        <div className={style.post_title}>
+          <h1>{post.title}</h1>
         </div>
-        <div className={style.post_author}>
-          <h3>Por C3S4R780</h3>
+        {post.excerpt && (
+          <div className={style.post_excerpt}>
+            <q>{post.excerpt}</q>
+          </div>
+        )}
+        <div className={style.post_thumbnail}>
+          <img src={thumbnail} alt={post.thumbnail?.data?.attributes.alternativeText} />
+        </div>
+        <div className={style.post_info}>
+          <div className={style.post_platforms}>
+            <h3>Plataformas:</h3>
+            {post.platforms && post.platforms?.data.map(platform => (
+              <Link key={`platform-${platform.id}`} to={`/platform/${platform.attributes.name.toLowerCase()}`}>
+                {platform.attributes.name}
+              </Link>
+            ))}
+          </div>
+          <div className={style.post_author}>
+            <h3>Por C3S4R780</h3>
+          </div>
+        </div>
+        <div className={style.post_content}>
+          <ReactMarkdown>
+            {post.content}
+          </ReactMarkdown>
         </div>
       </div>
-      <div className={style.post_content}>
-        <ReactMarkdown>
-          {post.content}
-        </ReactMarkdown>
+      <div>
+        <ShareSidebar />
       </div>
     </div>
   )
