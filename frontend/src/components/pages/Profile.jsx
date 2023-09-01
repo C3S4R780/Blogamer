7e2fdc3e10c280f48a1e8dacabb2e57eba82df26
Cesar Santos <C3S4R780@gmail.com>
2023-09-01 15:56:06 -0300

    [Add] Is now possible to enter other users profiles passing their ID / slug In the future, it will need additional validation to prevent users modifing other users profiles

--- frontend/src/components/pages/Profile.jsx
+++ frontend/src/components/pages/Profile.jsx
@@ -1,20 +1,41 @@
-import { useEffect } from "react"
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
 import { useAuthContext } from "../../context/AuthContext"
 import style from "./Profile.module.css"
+import { getToken } from "../../helper"
+import { API } from "../../constant"
 
 function Profile() {
-    const { user } = useAuthContext()
     const navigate = useNavigate()
+    const userToken = getToken()
+    const { user } = useAuthContext()
+    const { profileId } = useParams()
+    const [profile, setProfile] = useState()
 
     useEffect(() => {
-        !user?.id && navigate("/login", { replace: true })
+        !userToken && navigate("/login", { replace: true })
     })
+
+    useEffect(() => {
+        if (profileId) {
+            fetch(`${API}/users/${profileId}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(resp => resp.json())
+            .then(data => setProfile(data))
+            .catch(err => console.error(err))
+        } else {
+            setProfile(user)
+        }
+    }, [profileId, user])
 
     return (
         <div className={style.profile}>
             <div className={style.profile_username}>
-                <h2>{user?.username}</h2>
+                <h2>{profile?.username}</h2>
             </div>
         </div>
     )
