cabcb176e242a70f3a438cf80a4e1c415912eb26
Cesar Santos <C3S4R780@gmail.com>
2023-10-03 18:36:52 -0300

    [Add] Extra options for the CreatePost WYSIWYG (Images/Iframes)

--- frontend/src/components/layout/PostContentEditor.jsx
+++ frontend/src/components/layout/PostContentEditor.jsx
@@ -4,10 +4,33 @@ import 'react-quill/dist/quill.snow.css';
 import './PostContentEditor.css'
 
 function PostContentEditor({ value, handleContent }) {
-
+  const modules = {
+    toolbar: [
+      [{'header': [1, 2, 3, false]}],
+      [{'align': []}, 'bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}],
+      ['link', 'image', 'video'],
+      ['clean']
+    ],
+    clipboard: {
+      matchVisual: false,
+    }
+  }
+  const formats = [
+    'header',
+    'align', 'indent', 'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'ordered', 'bullet',
+    'link', 'image', 'video'
+  ]
   return (
     <div className='post_content_editor'>
-        <ReactQuill theme="snow" value={value} onChange={value => {handleContent(value)}} />
+        <ReactQuill
+          theme="snow"
+          value={value}
+          onChange={value => handleContent(value)}
+          modules={modules}
+          formats={formats}
+        />
     </div>
   )
 }
