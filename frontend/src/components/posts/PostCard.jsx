945f60157dd7f37d9cbe54f40a5557d056bf45e0
Cesar Santos <C3S4R780@gmail.com>
2023-11-09 14:30:58 -0300

    [Add] Post meta, such as author, date, number of likes(WIP) and comments inside Post Card

--- frontend/src/components/posts/PostCard.jsx
+++ frontend/src/components/posts/PostCard.jsx
@@ -2,11 +2,17 @@ import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import style from "./PostCard.module.css"
 
-import { FaArrowRight } from "react-icons/fa6"
+import { FaArrowRight, FaHeart } from "react-icons/fa6"
+import { BiSolidCommentDetail } from "react-icons/bi"
 
 function PostCard({ post, dark }) {
   const [thumbnail, setThumbnail] = useState(null)
   const postUrl = `/post/${post.attributes.slug}`
+
+  const getPostDate = (postDate) => {
+    const parsedDate = new Date(postDate)
+    return parsedDate.toLocaleString().split(' ')[0]
+  }
 
   useEffect(() => {
     if (post.attributes.thumbnail?.data) {
@@ -21,16 +27,32 @@ function PostCard({ post, dark }) {
           setThumbnail("/no-image.png")
         }
       })
+    } else {
+      setThumbnail('/no-image.png')
     }
   }, [thumbnail, post.attributes.thumbnail?.data])
 
   return (
-    <div className={`${style.post_card} ${dark && style.dark}`}>
-      <Link to={postUrl} className={style.post_card_img}>
-        {thumbnail && (
-          <img src={thumbnail} alt={post.attributes.thumbnail.data?.attributes.alternativeText} />
-        )}
-      </Link>
+    <div className={`${style.post_card} ${dark ? style.dark : ''}`}>
+      <div className={style.post_card_thumb}>
+        <Link to={postUrl} className={style.post_card_img}>
+            <img src={thumbnail} alt={post.attributes.thumbnail.data?.attributes.alternativeText} />
+        </Link>
+        <div className={style.post_card_meta}>
+          <div className={style.post_card_meta_section}>
+            <span>
+              <Link to={`/profile/${post.attributes.author.data.attributes.slug}`}>
+                {post.attributes.author.data.attributes.username}
+              </Link>
+            </span>
+            <span>{getPostDate(post.attributes.createdAt)}</span>
+          </div>
+          <div className={style.post_card_meta_section}>
+            <span>0<FaHeart /></span>
+            <span>{post.attributes.comments.data.length}<BiSolidCommentDetail /></span>
+          </div>
+        </div>
+      </div>
       <div className={style.post_card_content}>
         <div className={style.post_card_platforms}>
           {post.attributes?.platforms && post.attributes.platforms.data.map(platform => (
